a14604db6db1bb24578e3cd918babb28ee4c072d
Vyom Raturi <vyomraturi2004@gmail.com>
2024-05-23 22:45:26 +0530

    added constants

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
 import './App.css';
+import { useState, useEffect } from 'react';
+import { ethers } from 'ethers';
+import { contractAbi, contractAddress } from './Constant/constant';
 
 function App() {
   return (
