9a2fadd8f88c07424123f58ada6a096895af1bcd
Vyom Raturi <vyomraturi2004@gmail.com>
2024-05-23 22:55:41 +0530

    added components

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import './App.css';
 import { useState, useEffect } from 'react';
 import { ethers } from 'ethers';
 import { contractAbi, contractAddress } from './Constant/constant';
+import Login from './Components/Login';
+import Finished from './Components/Finished';
+import Connected from './Components/Connected';
 
 function App() {
   return (
